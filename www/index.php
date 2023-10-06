6f34d324c1e71c518bcec1a1fbfa529bd898588a
andersondinizdev <andersondiniz159@gmail.com>
2023-10-06 07:14:14 -0300

    style: Estilização da página de login.

---
+++ www/index.php
@@ -0,0 +1,31 @@
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/styles-login.css">
+  <link rel="stylesheet" href="styles/texts-color.css">
+  <link rel="stylesheet" href="styles/texts-types.css">
+  <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&display=swap" rel="stylesheet">
+  <title>Clinic Web</title>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="container d-flex justify-content-center align-items-center">
+      <div class="login-container d-flex justify-content-center align-items-center flex-column">
+        <h1 class="texts-color-white texts-type-1 d-flex align-items-center gap-3"><img src="assets/logo.svg" alt="logo-img" />Clinic Web</h1>
+        <form class="login-area">
+
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
