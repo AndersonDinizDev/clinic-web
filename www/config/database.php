d9bba79f4019b1124311380a03258f72dc3df2d1
Anderson Diniz <113947327+AndersonDinizDev@users.noreply.github.com>
2023-11-05 09:14:56 -0300

    Update database.php

--- www/config/database.php
+++ www/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-  $database = new PDO("mysql:host=db;dbname=plataforma-web", "admin", "admin");
+  $database = new PDO("mysql:host=db;dbname=master", "admin", "admin");
   $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $database;
 } catch (PDOException $err) {
